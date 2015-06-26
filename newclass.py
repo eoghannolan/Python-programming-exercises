47f767f85cc770bf40bb40ba94d5397ae44d2d9a
Eoghan Nolan <eoghan.nolan@gmail.com>
2015-06-26 15:50:27 +0100

    Updated question 5 Class and Methods

---
+++ newclass.py
@@ -0,0 +1,14 @@
+__author__ = 'Eoghan'
+
+class UpperCase:
+
+    def __init__(self, text):
+       self._text = text
+
+    def upper(self):
+        print(self._text.upper())
+
+    def output(self):
+        print(self._text)
+
+
